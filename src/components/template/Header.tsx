15adfc4694a0d640f2ff159549a4cd8faaf4a5d7
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2021-01-29 11:14:51 -0300

    go

--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -1,13 +1,15 @@
 import HeaderArea from '../../styles/template/HeaderArea'
+import { Home } from '@styled-icons/fa-solid'
+type HeaderProps = { icon: string; title: string; subtitle: string }
 
-type HeaderProps = { icon: string, title: string, subtitle: string }
-
-const Header: React.FC<HeaderProps> = ({ icon, title, subtitle }) =>
-    <HeaderArea className='d-none d-sm-flex flex-column'>
-        <h1 className='mt-3'>
-            <i className={`fa fa-${icon}`} /> {title}
-        </h1>
-        <p className='lead text-muted'>{subtitle}</p>
-    </HeaderArea>
+const Header: React.FC<HeaderProps> = ({ icon, title, subtitle }) => (
+  <HeaderArea className="d-none d-sm-flex flex-column">
+    <h1 className="mt-3">
+        <i className={`fa fa-${icon}`} /> {title} 
+        {/* <Home size="1.4rem" height="45px" />  */}
+    </h1>
+    <p className="lead text-muted">{subtitle}</p>
+  </HeaderArea>
+)
 
 export default Header

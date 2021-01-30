c9d504fb8359f26930423e4459077917e70aa466
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2021-01-30 17:19:17 -0300

    go

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
 import { AppProps } from 'next/app'
 
 import { ThemeProvider } from 'styled-components'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'font-awesome/css/font-awesome.min.css'
-
 import { Router } from 'react-router-dom'
 
-import Container from '../styles/pages/Container'
 import Footer from '../components/template/Footer'
+import Container from '../styles/pages/Container'
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
 import Home from '../components/main/Home'

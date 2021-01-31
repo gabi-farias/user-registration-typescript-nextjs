9b7b4f278b1f9f4d1663daef0646547a4616e14b
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2021-01-31 15:15:22 -0300

    go

--- src/components/users/UserCrud.tsx
+++ src/components/users/UserCrud.tsx
@@ -1,9 +1,7 @@
-import React, { Component, useState } from 'react'
+import React, { Component} from 'react'
 
 import Content from '../template/Content'
 import { User } from '../../../util'
-// import api from '../../server/api'
-
 import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/users'
@@ -41,137 +39,173 @@ export default class UserCrud extends Component<OwnProps, OwnSate> {
     this.setState({ user: initialState.user })
   }
 
-  save() {
-    const user = this.state.user
-    const method = user.id ? 'put' : 'post'
-    const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
-    axios[method](url, user).then(resp => {
-      const usersList = this.getUpdatedList(resp.data)
-      this.setState({ user: initialState.user, usersList })
-    })
-  }
+  save() {}
 
-  getUpdatedList(user: User, add = true) {
-    const usersList = this.state.usersList.filter(u => u.id !== user.id)
-    if (add) usersList.unshift(user)
-    return usersList
-  }
+  getUpdatedList(user: User, add = true) {}
 
-  updateField(event: React.ChangeEvent<HTMLInputElement>) {
-    const user = { ...this.state.user }
-    user[event.target.name] = event.target.value
-    this.setState({ user })
-  }
+  updateField(event: unknown) {}
 
-  renderForm() {
-    return (
-      <div className='form'>
-        <div className='row'>
-          <div className='col-12 col-md-6'>
-            <div className='form-group'>
-              <label>Nome</label>
-              <input
-                type='text'
-                className='form-control'
-                name='name'
-                value={this.state.user.name}
-                onChange={e => this.updateField(e)}
-                placeholder='Type it the user name...'
-              />
-            </div>
-          </div>
+  renderForm() {}
 
-          <div className='col-12 col-md-6'>
-            <div className='form-group'>
-              <label>E-mail</label>
-              <input
-                type='text'
-                className='form-control'
-                name='email'
-                value={this.state.user.email}
-                onChange={e => this.updateField(e)}
-                placeholder='Type it the user e-mail...'
-              />
-            </div>
-          </div>
-        </div>
+  load(user: User) {}
 
-        <hr />
-        <div className='row'>
-          <div className='col-12 d-flex justify-content-end'>
-            <button className='btn btn-primary' onClick={() => this.save()}>
-              Save
-            </button>
+  remove(user: User) {}
 
-            <button
-              className='btn btn-secondary ml-2'
-              onClick={() => this.clear()}
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
-      </div>
-    )
-  }
+  renderTable() {}
 
-  load(user: User) {
-    this.setState({ user })
-  }
-
-  remove(user: User) {
-    axios.delete(`${baseUrl}/${user.id}`).then(resp => {
-      const usersList = this.getUpdatedList(user, false)
-      this.setState({ usersList })
-    })
-  }
-
-  renderTable() {
-    return (
-      <table className='table mt-4'>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>E-mail</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>{this.renderRows()}</tbody>
-      </table>
-    )
-  }
-
-  renderRows() {
-    return this.state.usersList.map(user => {
-      return (
-        <tr key={user.id}>
-          <td>{user.id}</td>
-          <td>{user.name}</td>
-          <td>{user.email}</td>
-          <td>
-            <button className='btn btn-warning'
-              onClick={() => this.load(user)}>
-              <i className='fa fa-pencil'></i>
-            </button>
-            <button
-              className='btn btn-danger ml-2'
-              onClick={() => this.remove(user)}
-            >
-              <i className='fa fa-trash'></i>
-            </button>
-          </td>
-        </tr>
-      )
-    })
-  }
+  renderRows() {}
 
   render() {
     return (
       <Content {...headerProps}>
-        {this.renderForm()}
-        {this.renderTable()}
+        <div>
+          {this.state.usersList.map(user => (
+            <h1 className="d-flex justify-content-center" key={user.id}>
+              {user.name}
+            </h1>
+          ))}
+        </div>
       </Content>
     )
   }
 }
+
+
+// clear() {
+//   this.setState({ user: initialState.user })
+// }
+
+// save() {
+//   const user = this.state.user
+//   const method = user.id ? 'put' : 'post'
+//   const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+//   axios[method](url, user).then(resp => {
+//     const usersList = this.getUpdatedList(resp.data)
+//     this.setState({ user: initialState.user, usersList })
+//   })
+// }
+
+// getUpdatedList(user: User, add = true) {
+//   const usersList = this.state.usersList.filter(u => u.id !== user.id)
+//   if (add) usersList.unshift(user)
+//   return usersList
+// }
+
+// updateField(event: React.ChangeEvent<HTMLInputElement>) {
+//   const user = { ...this.state.user }
+//   user[event.target.name] = event.target.value
+//   this.setState({ user })
+// }
+
+// renderForm() {
+//   return (
+//     <div className='form'>
+//       <div className='row'>
+//         <div className='col-12 col-md-6'>
+//           <div className='form-group'>
+//             <label>Nome</label>
+//             <input
+//               type='text'
+//               className='form-control'
+//               name='name'
+//               value={this.state.user.name}
+//               onChange={e => this.updateField(e)}
+//               placeholder='Type it the user name...'
+//             />
+//           </div>
+//         </div>
+
+//         <div className='col-12 col-md-6'>
+//           <div className='form-group'>
+//             <label>E-mail</label>
+//             <input
+//               type='text'
+//               className='form-control'
+//               name='email'
+//               value={this.state.user.email}
+//               onChange={e => this.updateField(e)}
+//               placeholder='Type it the user e-mail...'
+//             />
+//           </div>
+//         </div>
+//       </div>
+
+//       <hr />
+//       <div className='row'>
+//         <div className='col-12 d-flex justify-content-end'>
+//           <button className='btn btn-primary' onClick={() => this.save()}>
+//             Save
+//           </button>
+
+//           <button
+//             className='btn btn-secondary ml-2'
+//             onClick={() => this.clear()}
+//           >
+//             Cancel
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// load(user: User) {
+//   this.setState({ user })
+// }
+
+// remove(user: User) {
+//   axios.delete(`${baseUrl}/${user.id}`).then(resp => {
+//     const usersList = this.getUpdatedList(user, false)
+//     this.setState({ usersList })
+//   })
+// }
+
+// renderTable() {
+//   return (
+//     <table className='table mt-4'>
+//       <thead>
+//         <tr>
+//           <th>ID</th>
+//           <th>Name</th>
+//           <th>E-mail</th>
+//           <th>Action</th>
+//         </tr>
+//       </thead>
+//       <tbody>{this.renderRows()}</tbody>
+//     </table>
+//   )
+// }
+
+// renderRows() {
+//   return this.state.usersList.map(user => {
+//     return (
+//       <tr key={user.id}>
+//         <td>{user.id}</td>
+//         <td>{user.name}</td>
+//         <td>{user.email}</td>
+//         <td>
+//           <button className='btn btn-warning'
+//             onClick={() => this.load(user)}>
+//             <i className='fa fa-pencil'></i>
+//           </button>
+//           <button
+//             className='btn btn-danger ml-2'
+//             onClick={() => this.remove(user)}
+//           >
+//             <i className='fa fa-trash'></i>
+//           </button>
+//         </td>
+//       </tr>
+//     )
+//   })
+// }
+
+// render() {
+//   return (
+//     <Content {...headerProps}>
+//       {this.renderForm()}
+//       {this.renderTable()}
+//     </Content>
+//   )
+// }
+// }

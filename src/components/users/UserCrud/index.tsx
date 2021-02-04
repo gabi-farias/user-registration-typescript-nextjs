23ba7e4a975d21c592c66d381244846b4d672f29
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2021-02-03 23:58:05 -0300

    refactoring

--- src/components/users/UserCrud/index.tsx
+++ src/components/users/UserCrud/index.tsx
@@ -116,46 +116,6 @@ const UserCrud: React.FC = () => {
     })
   }
 
-  const renderTable = () => {
-    return (
-      <table className="table mt-4">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Email</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>{renderRows()}</tbody>
-      </table>
-    )
-  }
-
-  const renderRows = () =>
-    state.usersList.map(user => {
-      return (
-        <tr key={user.id}>
-          <td>{user.id}</td>
-          <td>{user.name}</td>
-          <td>{user.email}</td>
-          <td>
-            <button className="btn btn-warning" onClick={() => load(user)}>
-              <i className="fa fa-pencil"></i>
-            </button>
-            <button
-              className="btn btn-danger ml-2"
-              onClick={() => remove(user)}
-            >
-              <i className="fa fa-trash"></i>
-            </button>
-          </td>
-        </tr>
-      )
-    })
-
-  console.log(state.usersList)
-
   return (
     <Content {...headerProps}>
       {renderForm()}

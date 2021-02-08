baff658654bb1e9464858c07c3ff69b9483fe2c1
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2021-02-08 17:43:47 -0300

    refactoring

--- src/components/users/UserTable/index.tsx
+++ src/components/users/UserTable/index.tsx
@@ -1,28 +1,21 @@
-import React from 'react'
+import TableButtons from './TableButtons'
 
 import { UserTableProps as OwnProps } from '../../../../util'
 
 const UserTable: React.FC<OwnProps> = ({ usersList, load, remove }) => {
   const renderUserRows = () =>
-  usersList.map(user => {
-    return (
-      <tr key={user.id}>
-        <td>{user.id}</td>
-        <td>{user.name}</td>
-        <td>{user.email}</td>
-        <td>
-          <button className="btn btn-warning ml-2"
-            onClick={() => load(user)}>
-            <i className="fa fa-pencil" />
-          </button>
-          <button className="btn btn-danger ml-2"
-            onClick={() => remove(user)}>
-            <i className="fa fa-trash" />
-          </button>
-        </td>
-      </tr>
-    )
-  })
+    usersList.map(user => {
+      return (
+        <tr key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          <td>
+            <TableButtons user={user} load={load} remove={remove} />
+          </td>
+        </tr>
+      )
+    })
 
   return (
     <table className="table mt-4">

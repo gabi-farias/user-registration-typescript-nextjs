c4a2a9b7a8d5d2e0d500d8bc5473bb7c956df1fb
mano-el48 <53446625+mano-el48@users.noreply.github.com>
2021-02-08 17:51:25 -0300

    refactoring

--- src/components/users/UserTable/index.tsx
+++ src/components/users/UserTable/index.tsx
@@ -2,34 +2,34 @@ import TableButtons from './TableButtons'
 
 import { UserTableProps as OwnProps } from '../../../../util'
 
-const UserTable: React.FC<OwnProps> = ({ usersList, load, remove }) => {
-  const renderUserRows = () =>
-    usersList.map(user => {
-      return (
-        <tr key={user.id}>
-          <td>{user.id}</td>
-          <td>{user.name}</td>
-          <td>{user.email}</td>
-          <td>
-            <TableButtons user={user} load={load} remove={remove} />
-          </td>
-        </tr>
-      )
-    })
-
-  return (
-    <table className="table mt-4">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>E-mail</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>{renderUserRows()}</tbody>
-    </table>
-  )
-}
+const UserTable: React.FC<OwnProps> = ({ usersList, load, remove }) => (
+  <table className="table mt-4">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>E-mail</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {usersList.map(user => {
+        return (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>
+              <TableButtons
+                user={user}
+                load={load}
+                remove={remove} />
+            </td>
+          </tr>
+        )
+      })}
+    </tbody>
+  </table>
+)
 
 export default UserTable

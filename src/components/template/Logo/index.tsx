e5f4dc8e7b20563e21d4ae41de3208713970b475
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-03-24 21:13:50 -0300

    passing the logo png file to the public folder on root and erasing the assets folder

--- src/components/template/Logo/index.tsx
+++ src/components/template/Logo/index.tsx
@@ -1,13 +1,12 @@
 import Link from 'next/link'
 
-import logo from '../../../assets/logo.png'
 import Container from './styles'
 
 const Logo: React.FC = () => (
   <Container>
     <Link href="/">
       <a>
-        <img src={logo} alt="logo" />
+        <img src="/logo.png" alt="logo" />
       </a>
     </Link>
   </Container>
